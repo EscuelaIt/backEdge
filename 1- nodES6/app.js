6f3e23a6ffe0b0375e52ba8d8eaa129e11dab097
Alberto Basalo <albertobasalo@hotmail.com>
2016-04-04 19:57:12 +0200

    el proceso

--- 1- nodES6/app.js
+++ 1- nodES6/app.js
@@ -61,3 +61,27 @@ promesas.descargando('agorabinaria.com')
 promesas.descargando('unrutaquenoexiste.es')
     .then(result => console.log(`Todo bien ${result.statusCode}`))
     .catch(reason => console.error(reason))
+
+
+// el proceso
+process.title = 'mi super aplicación back edge'
+
+imprimir()
+
+// Puedes conocer cosas acerca del proceso en curso
+function imprimir() {
+    console.log('----------------------------------------')
+    console.log('   DATOS DE NODE.JS PROCESS')
+    console.log('----------------------------------------')
+    console.log('Directorio .......... ' + process.cwd())
+    console.log('Exec path ........... ' + process.execPath)
+    //console.log('identificador uid ... ' + process.getuid())
+    console.log('identificador pid ... ' + process.pid)
+    console.log('Título .............. ' + process.title)
+    console.log('Versión de Node  .... ' + process.version)
+    console.log('Plataforma........... ' + process.platform)
+}
+
+// tests
+// npm install mocha -g 
+// mocha
